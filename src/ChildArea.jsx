7a4ed33b903d9d669daffe36212661fa02161f09
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2021-12-25 15:29:47 +0900

    111

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,10 +1,12 @@
+import { memo } from "react";
+
 const style = {
   width: "100%",
   height: "200px",
   backgroundColor: "khaki"
 };
 
-export const ChildArea = (props) => {
+export const ChildArea = memo((props) => {
   const { open } = props;
   console.log("チャイルドエリアがレンダリングされた！");
   const data = [...Array(2000).keys()];
@@ -23,4 +25,4 @@ export const ChildArea = (props) => {
       ) : null}
     </>
   );
-};
+});
